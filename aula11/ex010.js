b760a149f8d3da24441f3f1214b133312faf2fcd
Amanda Fernandes <124813517+amandagpf@users.noreply.github.com>
2023-03-23 16:45:59 -0300

    Commit aula 11

---
+++ aula11/ex010.js
@@ -0,0 +1,7 @@
+var país = 'França'
+console.log(`Vivendo em ${país}`)
+if(país == 'Brasil'){//aqui temos condição COMPOSTA
+    console.log('Você é BRASILEIRO!')
+}else{
+    console.log('Você é ESTRANGEIRO!')
+}
